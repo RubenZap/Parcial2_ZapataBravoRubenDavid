e400490356a45d202f82c8a1a39514ee870d6ea7
RUBEN DAVID ZAPATA BRAVO <rubenzapata264702@correo.itm.edu.co>
2023-04-02 15:16:13 -0500

    validation id

--- ConcertDB/ConcertDB/Controllers/TicketsController.cs
+++ ConcertDB/ConcertDB/Controllers/TicketsController.cs
@@ -72,11 +72,11 @@ namespace ConcertDB.Controllers
                 {
                     if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
-                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+                        ModelState.AddModelError(string.Empty, "Boleta Valida");
                     }
                     else
                     {
-                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                        ModelState.AddModelError(string.Empty, "Boleta no Valida");
                     }
                 }
                 catch (Exception exception)
